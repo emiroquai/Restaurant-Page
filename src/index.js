2832f142c7a89e75cbb62403ee5d2b3e09d247ab
emiroquai <22440776+emiroquai@users.noreply.github.com.>
2023-10-20 19:24:16 +0200

    Basic page setup with DOM manip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import './style.css';
 import './script.js';
+import './style.css';
 
--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-console.log("Hellooowwww");
 const content = document.getElementById("content");
 
 // Create header
@@ -8,9 +7,36 @@ header.classList.add('header');
 title.classList.add('title');
 title.textContent = "Best Restaurant Ever"
 
+// Create Nav
+const nav = document.createElement('div');
+nav.classList.add('nav');
+const home = document.createElement('button');
+home.setAttribute("id", "home");
+home.textContent = "Home";
+const menu = document.createElement('button');
+menu.setAttribute("id", "menu");
+menu.textContent = "Menu";
+const contact = document.createElement('button');
+contact.setAttribute("id", "contact");
+contact.textContent = "Contact";
+
+
 // Create main
+const main = document.createElement('div');
+const info = document.createElement('h2');
+info.classList.add('info');
+info.textContent = "Join us for the best burgers in the universe and the 3rd best fries in the milkyway!"
 
 // Append children
 header.appendChild(title);
+
+nav.appendChild(home);
+nav.appendChild(menu);
+nav.appendChild(contact);
+
+main.appendChild(info);
+
 content.appendChild(header);
+content.appendChild(nav);
+content.appendChild(main);
 
